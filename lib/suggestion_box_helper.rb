fbd871f876e5ac29691119011776f3d20d6ca722
Eric Berry <cavneb@gmail.com>
2008-10-03 16:22:28 -0600

    ADDED ALL

---
+++ lib/suggestion_box_helper.rb
@@ -0,0 +1,12 @@
+module SuggestionBoxHelper
+  def suggestion_box(application_name)
+    config = File.open("#{RAILS_ROOT}/config/suggestion_box.yml") 
+    options = YAML.load(config)
+    f = File.open("#{RAILS_ROOT}/vendor/plugins/suggestion_box/lib/template.html")
+    ret = ""
+    f.each do |line|
+      ret << line.gsub('[[application_name]]'){application_name}.gsub('[[url]]'){config[:url]}
+    end
+    return ret
+  end
+end
